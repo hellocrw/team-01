f0e5eb808be74e82aaeb4115c699cab59dbb1cbd
caorongwu <caorongwu@ly-sky.com>
2020-10-15 18:33:55 +0800

    rabbitmq消息队列

--- src/main/java/crw/bishe/team/controller/rabbitmq/Customer.java
+++ src/main/java/crw/bishe/team/controller/rabbitmq/Customer.java
@@ -1,21 +1,73 @@
 package crw.bishe.team.controller.rabbitmq;
 
 import io.swagger.annotations.Api;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = {"消息消费者"})
-@RabbitListener(queuesToDeclare = @Queue("hello"))
 public class Customer {
 
-    @GetMapping("receivel")
+    // hello world模型
+    @RabbitListener(queuesToDeclare = @Queue("hello"))
     @RabbitHandler
     public void receivel(String message){
         System.out.println("message = " + message);
+    }
+
+    // queue work模型
+    @RabbitListener(queuesToDeclare = @Queue("queue work"))
+    public void receivel1(String message) {
+        System.out.println("work message1 = " + message);
+    }
+
+    // queue work模型
+    @RabbitListener(queuesToDeclare = @Queue("queue work"))
+    public void receivel2(String message) {
+        System.out.println("work message2 = " + message);
+    }
+
+    // Fanout 广播模型
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue,
+            exchange = @Exchange(name = "logs", type = "fanout")
+    ))
+    public void fanoutCustomer1(String message){
+        System.out.println("fanoutCustomer1 => " + message);
+    }
+
+    // Fanout 广播模型
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue,
+            exchange = @Exchange(name = "logs", type = "fanout")
+    ))
+    public void fanoutCustomer2(String message){
+        System.out.println("fanoutCustomer2 => " + message);
+    }
+
+    // Route 路由模型
+    @RabbitListener(bindings = {
+            @QueueBinding(
+                    value = @Queue(),
+                    key = {"info", "error"},
+                    exchange = @Exchange(type = "direct", name="directs")
+            )
+    })
+    public void routeCostomer1(String message){
+        System.out.println("routeCostomer1 => " + message);
+    }
+
+    // Route 路由模型
+    @RabbitListener(bindings = {
+            @QueueBinding(
+                    value = @Queue(),
+                    key = {"error"},
+                    exchange = @Exchange(type = "direct", name="directs")
+            )
+    })
+    public void routeCostomer2(String message){
+        System.out.println("routeCostomer2 => " + message);
     }
 
 }
--- src/main/java/crw/bishe/team/controller/rabbitmq/Provider.java
+++ src/main/java/crw/bishe/team/controller/rabbitmq/Provider.java
@@ -13,8 +13,29 @@ public class Provider {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    // hello world模型
     @GetMapping("/dirctProvider")
     public void directMessage(){
         rabbitTemplate.convertAndSend("hello", "hello world");
+    }
+
+    // queue work模型
+    @GetMapping("/queueWork")
+    public void queueWork(){
+        for (int i = 0; i < 10; i++) {
+            rabbitTemplate.convertAndSend("queue work", "hello world");
+        }
+    }
+
+    // Fanout 广播模型
+    @GetMapping("/fanout")
+    public void testFanout() throws InterruptedException{
+        rabbitTemplate.convertAndSend("logs", "", "这事日志广播");
+    }
+
+    // Route 路由模型
+    @GetMapping("/routeProdiver")
+    public void routeProdiver(){
+        rabbitTemplate.convertAndSend("directs","error","error 的日志信息");
     }
 }
